c2e1596c0898969e3b92d70b8c10dce53576f095
ChristianAyala <chr.ayala@gmail.com>
2020-07-25 15:08:38 -0500

    Linting, prettier, updated tsconfig and folder structure

---
+++ ehoh-mobile/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['universe/native', 'universe/shared/typescript-analysis'],
+  ignorePatterns: ['App.ts'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  ]
+};
--- ehoh-mobile/babel.config.js
+++ ehoh-mobile/babel.config.js
@@ -1,6 +1,12 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.svg']
+      }
+    ]
+  ]
 };
