8f874b871311409baec98aea8885d4cd881e2788
ChristianAyala <chr.ayala@gmail.com>
2020-07-27 11:01:08 -0500

    Committing and pushing current work for now

--- ehoh-mobile/src/store/auth/sagas.ts
+++ ehoh-mobile/src/store/auth/sagas.ts
@@ -1,19 +1,21 @@
+import * as Google from 'expo-google-app-auth';
 import { put, takeLatest } from 'redux-saga/effects';
 import firebase from 'services/firebase';
 
 import { actions, actionTypes } from './reducer';
 
+const onSuccessfulLogin = (user: Google.LogInResult) => {};
+
 export function* loginWithGoogle() {
   yield put(actions.loginLoading(true));
 
-  const authProvider = new firebase.auth.GoogleAuthProvider();
+  // const authProvider = new firebase.auth.GoogleAuthProvider();
 
   try {
-    yield firebase.auth().signInWithRedirect(authProvider);
-    const result: firebase.auth.UserCredential = yield firebase.auth().getRedirectResult();
-    if (result.user && result.credential) {
-      yield put(actions.loginSuccess(result.user, result.credential));
-    }
+    const result: Google.LogInResult = yield Google.logInAsync({
+      iosClientId: '66608434815-617fknoia86venclf1g4jt99hkg03l07.apps.googleusercontent.com'
+    });
+    console.log(result);
   } catch (error) {
     yield put(actions.loginError(error));
   } finally {
